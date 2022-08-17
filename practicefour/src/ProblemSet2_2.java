e14cf97e89b8a09f951bc754de3e0194731253da
Jennamaslovich <108747993+Jennamaslovich@users.noreply.github.com>
2022-08-17 10:43:35 -0400

    practice four

---
+++ practicefour/src/ProblemSet2_2.java
@@ -0,0 +1,23 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+           System.out.println(" .-.     .-. ");
+           System.out.println(" .****. .****. ");
+         System.out.println(" .*****.*****. ");
+           System.out.println(" .*********. ");
+             System.out.println(" .*******. ");
+             System.out.println(" .*****. ");
+                 System.out.println(" .***. ");
+                     System.out.println("   *  ");
+
+
+        // its a wonky heart
+        
+        
+        
+        
+        
+        
+        
+        
+    } 
+}
